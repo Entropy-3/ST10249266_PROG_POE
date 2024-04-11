d2b82c8c8229477582b2e85a6f60e9fe0426850c
Nathan Teixeira <st10249266@vcconnect.edu.za>
2024-04-11 10:21:43 +0200

    Created variables in ingredient class as well as getters and setters and creating an ingredient object

--- Classes/Ingredients.cs
+++ Classes/Ingredients.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ST10249266_PROG_POE.Classes
+﻿namespace ST10249266_PROG_POE.Classes
 {
     internal class Ingredients
     {
+        private string ingredientName;
+        private float ingredientQuantity;
+        private string ingredientMeasurement;
+
+        //getters and setters for the ingredient name, quantity and measurement
+        public string IngredientName { get => ingredientName; set => ingredientName = value; }
+
+        public float IngredientQuantity { get => ingredientQuantity; set => ingredientQuantity = value; }
+        public string IngredientMeasurement { get => ingredientMeasurement; set => ingredientMeasurement = value; }
+
+        //----------------------------------------\\
+        //defualt constructor
+        private Ingredients()
+        {
+        }
+
+        //----------------------------------------\\
+        //constructor that takes in the ingredient name, quantity and measurement
+        public Ingredients(string ingredientName, float ingredientQuantity, string ingredientMeasurement)
+        {
+            this.ingredientName = ingredientName;
+            this.ingredientQuantity = ingredientQuantity;
+            this.ingredientMeasurement = ingredientMeasurement;
+        }
     }
 }
