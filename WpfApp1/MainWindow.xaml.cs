2517d5b7db23e4f1d3805a9706e941558a2f695b
Nathan Teixeira <130435264+Entropy-3@users.noreply.github.com>
2024-06-25 12:28:34 +0200

    finished UI for create recipe

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp1.Windows;
 
 namespace WpfApp1
 {
@@ -20,5 +21,18 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        // When the CreateRecipe button is clicked
+        //opens the CreateRecipeWindow and hides the main window
+        private void CreateRecipe_Click(object sender, RoutedEventArgs e)
+        {
+            // Create and show the CreateRecipeWindow
+            createRecipeWindow createRecipeWindow = new createRecipeWindow();
+            createRecipeWindow.Show();
+
+            // Hide the MainWindow
+            this.Hide();
+        }
+
     }
 }
